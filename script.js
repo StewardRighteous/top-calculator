75ecc44fb1da5b39c3049f9925dd8483fa8ab9e5
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-07-10 14:10:55 +0530

    fix - refactor code to add functionality with object

--- script.js
+++ script.js
@@ -42,37 +42,139 @@ function operate(num1, operator, num2 ){
     return value;
 }
 
+/*CALCULATOR ALGORITHM
+    i/o :
+            0 + = --> ERROR
+            1+1 = --> End Value
+            1+1 + --> 2 (end Value) and addnew Num
+            1+1 - 2 = 0 --> end value
+    Procedure:
+        operands = {first, second}
+        operators = {operation}
+        
+        CLICK = 
+            IF operands.length != 2 ERROR
+            IF operands.length == 2 THEN operate()
+        
+        CLICK +,-,*,/
+            IF operators.length == 0 THEN 
+                push(operation)
+                first = value
+                second = null
+            IF operators.length != 0 THEN 
+                value = operate()
+                first = value
+                second = null  
+                push(operation)
+        CLICK C
+            operands = [first, second]
+            operators = []
+
+        Click anynum
+            UPDATE second = {second}{enteredValue}
+            OUTPUT--> second
+ */
+
 function calculator(){
-    // initializing values
-    let num1 = "";
-    let num2 = "";
-    const operations = ["+","-","*","/"]
-    let operator = "ERROR";
-    inputValue.value = "0";
+    let operands = {
+        first : "",
+        second : "",
+        getFirst(){
+            return parseInt(this.first);
+        },
+        getSecond(){
+            return parseInt(this.second);
+        },
+        setDefault(){
+            this.first = "";
+            this.second = "";
+        },
+        updateSecond(value){
+            this.second = value;
+        }, 
+        updateFirst(value){
+            this.first = value;
+        },
+        isEmpty(){
+            if(this.first = ""|| (this.second = "")){
+                return true
+            }
+            return false;
+        }
 
-    buttons.forEach(button => {
+    };
+    let operators = {
+        operator : "",
+        getOperator(){
+            return this.operator;
+        },
+        isEmpty(){
+            if (this.operator == ""){
+                return true;
+            }
+            return false;
+        },
+        setDefault(){
+            this.operator = "";
+        },
+        error(){
+            this.operator = "ERROR";
+        },
+        updateOperator(operator){
+            this.operator = operator;
+        },
+    }
+
+  
+    inputValue.value = "0"
+
+    buttons.forEach((button)=>{
         button.addEventListener("click", (e)=>{
-            let buttonFunction = e.target.textContent;
+            let selectedButton = e.target.textContent;
 
-            // executing operations depending on the clicked button
-            if(buttonFunction == "="){
-                inputValue.value = operate(parseInt(num1), operator, parseInt(num2));
-            }else if(operations.includes(buttonFunction)){
-                operator = buttonFunction;
-                num1 = inputValue.value;
-                num2 = "";
-                inputValue.value = num2;
-            }else if(buttonFunction == "C"){
-                num1 = "";
-                num2 = "";
-                operator = "ERROR";
-                inputValue.value = "0";
-            }else{
-                num2 = `${num2}${buttonFunction}`; 
-                inputValue.value = num2;
+            switch(selectedButton){
+
+                case "=":
+                    if(operators.isEmpty()){
+                        operators.error();
+                        inputValue.value = operators.getOperator();
+                    }else{
+                        inputValue.value = operate(operands.getFirst(), operators.operator, operands.getSecond());
+                        operators.setDefault();
+                        operands.first = inputValue.value;
+                        operands.second = "";
+                    }
+                    break;
+
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                        if(operators.isEmpty()){
+                            operators.updateOperator(selectedButton);
+                            operands.updateFirst(inputValue.value);
+                            operands.updateSecond(""); 
+                            inputValue.value = operands.second;
+                        }else{
+                            inputValue.value = operate(operands.getFirst(), operators.operator, operands.getSecond());
+                            operators.updateOperator(selectedButton);
+                            operands.updateFirst(inputValue.value);
+                            operands.updateSecond(""); 
+                        }
+                    break;
+
+                case "C":
+                    operands.setDefault();
+                    operators.setDefault();
+                    inputValue.value = "0"
+                    break;
+
+                default:
+                    operands.updateSecond(`${operands.second}${selectedButton}`);
+                    inputValue.value = operands.second;
             }
         })
-    });
+    })
 }
 
 calculator();

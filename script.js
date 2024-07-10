33e56d5902241f4dd846d23aa1cd4608d99bc04e
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-07-10 10:20:30 +0530

    feat - add functionality to calculator
    - add operations to different buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("button");
+const inputValue = document.querySelector("#input-area");
 
 function add(a,b){
     return a+b;
@@ -42,15 +43,34 @@ function operate(num1, operator, num2 ){
 }
 
 function calculator(){
-    let num1 = "0";
-    let num2;
+    // initializing values
+    let num1 = "";
+    let num2 = "";
+    const operations = ["+","-","*","/"]
     let operator = "ERROR";
+    inputValue.value = "0";
 
-    
     buttons.forEach(button => {
         button.addEventListener("click", (e)=>{
             let buttonFunction = e.target.textContent;
-            
+
+            // executing operations depending on the clicked button
+            if(buttonFunction == "="){
+                inputValue.value = operate(parseInt(num1), operator, parseInt(num2));
+            }else if(operations.includes(buttonFunction)){
+                operator = buttonFunction;
+                num1 = inputValue.value;
+                num2 = "";
+                inputValue.value = num2;
+            }else if(buttonFunction == "C"){
+                num1 = "";
+                num2 = "";
+                operator = "ERROR";
+                inputValue.value = "0";
+            }else{
+                num2 = `${num2}${buttonFunction}`; 
+                inputValue.value = num2;
+            }
         })
     });
 }
